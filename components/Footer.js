357336531e9b3796324fca3443fbe3ce3bde80fa
Jaedson Alves <jaedsom.alves@gmail.com>
2021-11-26 20:11:08 -0300

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,39 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-x-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+            <div>How Airbnb works</div>
+            <div>Newsroom</div>
+            <div>Investors</div>
+            <div>Airbnb Luxe</div>
+            </div>
+
+            <div className="space-x-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+            <div>How Airbnb works</div>
+            <div>Newsroom</div>
+            <div>Investors</div>
+            <div>Airbnb Luxe</div>
+            </div>
+
+            <div className="space-x-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+            <div>How Airbnb works</div>
+            <div>Newsroom</div>
+            <div>Investors</div>
+            <div>Airbnb Luxe</div>
+            </div>
+
+            <div className="space-x-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+            <div>How Airbnb works</div>
+            <div>Newsroom</div>
+            <div>Investors</div>
+            <div>Airbnb Luxe</div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
