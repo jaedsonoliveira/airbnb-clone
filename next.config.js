cb96306271179825c477939d0f15596ef9ff7d19
Jaedson Alves <jaedsom.alves@gmail.com>
2021-11-29 18:40:09 -0300

    third commit

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports={
     images:{
         domains: ['links.papareact.com'],
     },
+    env:{
+        mapbox_key: 'pk.eyJ1IjoiamFlZHNvbiIsImEiOiJja3dsM3d1d2swMmxnMndudzdjMjJxeXZhIn0.McXkbiDwfDRgoIcUQDeHHA'
+    }
 };
